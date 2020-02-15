563492e64a0794756db9c4e93c682251f23082b6
Drew Rawitz <email@drewrawitz.com>
2020-02-15 12:24:30 -0700

    initial commit

---
+++ src/example.js
@@ -0,0 +1,13 @@
+const username = 'freddy'
+
+if (typeof username === 'string') {
+  console.log('yup!')
+}
+
+if (!('serviceWorker' in navigator)) {
+  // you have an old browser :-(
+}
+
+const greeting = 'hello'
+console.log(`${greeting} world!`)
+;[1, 2, 3].forEach(x => console.log(x))
